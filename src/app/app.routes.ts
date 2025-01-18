34f8e7068809e4de9e1da76bc4430e7ffbf06460
haiderali015 <ha347278@gmail.com>
2025-01-18 19:04:07 +0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CarsComponent } from './pages/cars/cars.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo : 'login',
+        pathMatch : 'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'layout',
+        component:LayoutComponent,
+        children: [
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'cars',
+                component:CarsComponent
+            },
+            {
+                path:'booking',
+                component:BookingsComponent
+            }
+        ]
+    },
+];
